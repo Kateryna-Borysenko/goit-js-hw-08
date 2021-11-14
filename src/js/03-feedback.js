d7cf4de490681546735965b37f97f2d09bd784bf
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2021-11-14 15:16:11 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,48 +1,53 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state'; 
 
-const STORAGE_KEY = 'feedback-form-state';
 const refs = {
     form: document.querySelector('.feedback-form'),
     email: document.querySelector('.feedback-form input[name ="email"]'),
-    textarea: document.querySelector('.feedback-form  textarea[name="message"]'),
-    button: document.querySelector('.feedback-form button')
+    textarea: document.querySelector('.feedback-form  textarea[name="message"]')
 };
-
+// console.log(refs.form, refs.email, refs.textarea, refs.button);
 
 refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener("input", throttle(onTargetInput, 500));
 
-fillinTextfields();
+fillinTextfields(); 
 
 const formData = {};
 
 
 function onFormSubmit(e) {
     e.preventDefault();
-    // console.log('Отправляем форму');
+    console.log('Отправляем форму');
 
     e.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
+    localStorage.removeItem(STORAGE_KEY);  
 }
-
 
 function fillinTextfields() {
 
-    const savedText = localStorage.getItem(STORAGE_KEY);
-    console.log(savedText);
+    const savedText = localStorage.getItem(STORAGE_KEY); 
+    console.log(savedText); 
     if (savedText) {
-        const objSavedText = JSON.parse(savedText);
-
+        const objSavedText = JSON.parse(savedText); 
+        // console.log(objSavedText);
+        // console.log(objSavedText.email);
+        // console.log(objSavedText.message);
         refs.email.value = objSavedText.email;
-        refs.textarea.value = objSavedText.message;
+        refs.textarea.value = objSavedText.message
     }
 
 }
 
-refs.form.addEventListener('input', e => {
+//data for test
+//borysenko@gmail.com
+// Hello from feedback! ...test
 
+
+function onTargetInput (e) {
+ 
     formData[e.target.name] = e.target.value;
-
-    throttle(localStorage.setItem(STORAGE_KEY, JSON.stringify(formData)), 500);
-
-})
+   
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
